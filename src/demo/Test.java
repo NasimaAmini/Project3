ed0497a50c7f984cd6f5d82608d6a7a047f483aa
nasim <nasim@Nasima>
2023-04-17 00:02:15 -0400

    added new print

--- src/demo/Test.java
+++ src/demo/Test.java
@@ -4,6 +4,9 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("print");
 		System.out.println("abc");
+		
+		
+		System.out.println("MMMMMMMMMMMMMMMMMMMMM");
 	}
 
 }
